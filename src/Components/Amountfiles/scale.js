11cb197e98903b667d2ae299dd1e721c36f07b3b
vsangavi <vsangavi16@gmail.com>
2020-09-15 22:32:34 +0530

    adding test files

--- src/Components/Amountfiles/scale.js
+++ src/Components/Amountfiles/scale.js
@@ -1,5 +1,54 @@
-import { followersMarks } from "./followersMarks";
-export const scale = (value) => {
+//import { followersMarks } from "./followersMarks";
+const followersMarks = [
+  {
+    value: 0,
+    scaledValue: 10000,
+    label: "10k",
+  },
+  {
+    value: 25,
+    scaledValue: 50000,
+    label: "50k",
+  },
+  {
+    value: 50,
+    scaledValue: 100000,
+    label: "100k",
+  },
+  {
+    value: 75,
+    scaledValue: 250000,
+    label: "250k",
+  },
+  {
+    value: 100,
+    scaledValue: 500000,
+    label: "500k",
+  },
+  {
+    value: 125,
+    scaledValue: 1000000,
+    label: "1M",
+  },
+  {
+    value: 150,
+    scaledValue: 5000000,
+    label: "5M",
+  },
+  {
+    value: 175,
+    scaledValue: 7500000,
+    label: "7.5M",
+  },
+  {
+    value: 200,
+    scaledValue: 10000000,
+    label: "10M",
+  },
+];
+
+let value = 22;
+const scale = (value) => {
   const previousMarkIndex = Math.floor(value / 25);
   const previousMark = followersMarks[previousMarkIndex];
   const remainder = value % 25;
@@ -10,3 +59,4 @@ export const scale = (value) => {
   const increment = (nextMark.scaledValue - previousMark.scaledValue) / 25;
   return remainder * increment + previousMark.scaledValue;
 };
+console.log(scale(value));
